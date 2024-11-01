ed2f314cb98966f1ab94c1f185d4627fe42819e5
chachacollins <collinschacha@hotmail.com>
2024-11-01 21:03:40 +0300

    made get screen styling

---
+++ src/color.zig
@@ -0,0 +1,7 @@
+pub const rose = 0xebbcba;
+pub const text = 0xe0def4;
+pub const overlay = 0x26233a;
+pub const iris = 0xc4a7e7;
+pub const pine = 0x31748f;
+pub const gold = 0xf6c177;
+pub const love = 0xeb6f92;
